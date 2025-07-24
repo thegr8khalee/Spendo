0094ced7e1a85eaddf5e13771fd60443292f20fa
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2025-07-24 15:49:47 +0300

    API

---
+++ mobile/app/(root)/index.jsx
@@ -0,0 +1,36 @@
+import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo';
+import { Link } from 'expo-router';
+import { Text, View } from 'react-native';
+import { SignOutButton } from '@/components/SignOutButton';
+import { useTransactions } from '../../hooks/useTransactions';
+import { useEffect } from 'react';
+
+export default function Page() {
+  const { user } = useUser();
+  const { transactions, summary, isLoading, loadData, deleteTransaction } =
+    useTransactions(user.id);
+
+  useEffect(() => {
+    loadData();
+  }, [loadData]);
+
+  console.log('transactions:', transactions);
+  console.log('summary', summary);
+  console.log(user.id)
+  return (
+    <View>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+        <SignOutButton />
+      </SignedIn>
+      <SignedOut>
+        <Link href="/(auth)/sign-in">
+          <Text>Sign in</Text>
+        </Link>
+        <Link href="/(auth)/sign-up">
+          <Text>Sign up</Text>
+        </Link>
+      </SignedOut>
+    </View>
+  );
+}
