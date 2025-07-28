da50a79a1c74b072ac878127a269322ad19bb4d9
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2025-07-28 11:56:40 +0300

    mm

--- mobile/hooks/useTransactions.js
+++ mobile/hooks/useTransactions.js
@@ -4,7 +4,7 @@ import { useCallback, useState } from 'react';
 import { Alert } from 'react-native';
 // import { API_URL } from '../constants/api';
 
-const API_URL = "https://spendo-api-mxsr.onrender.com/api";
+const API_URL = "https://spendo-api-d6yj.onrender.com/api";
 
 export const useTransactions = (userId) => {
   const [transactions, setTransactions] = useState([]);
