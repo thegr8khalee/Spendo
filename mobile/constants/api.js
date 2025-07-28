4dd768da21a139e42cdab5d284134823d3244d14
thegr8khalee <125562939+thegr8khalee@users.noreply.github.com>
2025-07-28 11:11:14 +0300

    mm

--- mobile/constants/api.js
+++ mobile/constants/api.js
@@ -1,2 +1,2 @@
-export const API_URL = "https://spendo-api-mxsr.onrender.com/api";
+export const API_URL = "https://spendo-api-d6yj.onrender.com/api";
 // export const API_URL = "http://localhost:5001/api";
